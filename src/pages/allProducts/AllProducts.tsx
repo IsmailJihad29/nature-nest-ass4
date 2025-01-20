80be61d60409015d64b06ecb009ace714f4a46c3
ismailJihad29 <ismailhossenjihad234@gmail.com>
2025-01-20 20:04:22 +0600

    added paginations at all product page

--- src/pages/allProducts/AllProducts.tsx
+++ src/pages/allProducts/AllProducts.tsx
@@ -2,6 +2,9 @@
 import Loading from "@/components/Loading";
 import ProductCard from "@/components/ProductCard";
 import { useGetProductsQuery } from "@/redux/api/productApi";
+import { setCurrentPage } from "@/redux/features/paginationSlice";
+import { RootState } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
 
 const AllProducts = () => {
   const {
@@ -10,6 +13,34 @@ const AllProducts = () => {
     isError,
   } = useGetProductsQuery({});
 
+  const dispatch = useDispatch();
+  const currentPage = useSelector(
+    (state: RootState) => state.pagination.currentPage
+  );
+  const productsPerPage = 12;
+
+  // Logic for displaying products for the current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products?.data?.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // Logic for pagination controls
+  const totalPages = Math.ceil(products?.data?.length / productsPerPage);
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      dispatch(setCurrentPage(currentPage + 1));
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      dispatch(setCurrentPage(currentPage - 1));
+    }
+  };
   if (isLoading) {
     return (
       <Loading/>
@@ -28,9 +59,37 @@ const AllProducts = () => {
     <section className="py-12 min-h-screen flex flex-col items-center global-bg">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className=" grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full mx-auto">
-          {products?.data?.map((product: any) => (
+          {currentProducts?.map((product: any) => (
             <ProductCard product={product} key={product._id} />
           ))}
+           
+        </div>
+        <div className="flex justify-center items-center mt-8 space-x-4">
+          <button
+            onClick={prevPage}
+            disabled={currentPage === 1}
+            className={`secondary-btn ${
+              currentPage === 1
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-green-600"
+            }`}
+          >
+            Previous
+          </button>
+          <span className="text-lg font-semibold text-gray-700">
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            onClick={nextPage}
+            disabled={currentPage === totalPages}
+            className={`primary-btn ${
+              currentPage === totalPages
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-green-600"
+            }`}
+          >
+            Next
+          </button>
         </div>
       </div>
     </section>
